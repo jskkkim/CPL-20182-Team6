0e6433f431237ea052792a5cd71bbace38b0d7c1
슬기~ <43199300+myabcc17@users.noreply.github.com>
2018-10-30 04:46:12 +0900

    Add files via upload

---
+++ Visual_Contents/Scripts/DataController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataController : MonoBehaviour
+{
+    private static DataController instance;
+    public static DataController GetInstance() // Instance 받아오기.
+    {
+        if (instance == null)
+        {
+            instance = FindObjectOfType<DataController>();
+
+            if (instance == null)
+            {
+                GameObject container = new GameObject("DataController");
+
+                instance = container.AddComponent<DataController>();
+            }
+        }
+        return instance;
+    }
+    static private int score = 0;
+    static private int stage = 0;
+    static private float total_time = 0;
+
+    void Awake() // DataController가 실행 됐을 때, 내부 PlayerPrefs에 저장 되어 있는 데이터들을 읽음
+    {
+        score = PlayerPrefs.GetInt("Score");
+        stage = PlayerPrefs.GetInt("Stage");
+        total_time = PlayerPrefs.GetFloat("Total_time");
+    }
+
+    public void Add_Score()
+    {
+        Debug.Log("점수획득!");
+        score += 1;
+        PlayerPrefs.SetInt("Score", score);
+        PlayerPrefs.Save();
+    }
+    public int Get_score()
+    {
+        return score;
+    }
+    public void Set_Stage(int num)
+    {
+        PlayerPrefs.SetInt("Stage", num);
+        PlayerPrefs.Save();
+    }
+    public int Get_Stage()
+    {
+        return stage;
+    }
+    public void Add_time(float time)
+    {
+        total_time += time;
+        PlayerPrefs.SetFloat("Total_time", total_time);
+        PlayerPrefs.Save();
+    }
+    public float Get_AVGTime()
+    {
+        return total_time / (float)score;
+    }
+}
---
+++ Visual_Contents/Scripts/timelimit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class timelimit : MonoBehaviour {
+
+    private float LimitTime = 20f;
+    public Text text_Timer;
+    public Text Score;
+    private DataController DataController;
+
+    public Func<Scene> Scene { get; private set; }
+
+    void Start () {
+        DataController = DataController.GetInstance();
+    }
+	
+
+	void Update () {
+        LimitTime -= Time.deltaTime;
+        text_Timer.text = "시간 : " + Mathf.Round(LimitTime*10)/10;
+        Score.text = "점수 : " + DataController.Get_score();
+        if(Mathf.Round(LimitTime) <= 0)
+        {
+            Debug.Log("평균반응시간 : " + DataController.Get_AVGTime());
+            int stage = SceneManager.GetActiveScene().buildIndex;
+            PlayerPrefs.SetInt("Stage", stage);
+            PlayerPrefs.Save();
+            SceneChange2();
+        }
+    }
+    public void SceneChange2()
+    {
+        SceneManager.LoadScene("Result");
+    }
+}
